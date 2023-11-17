4b8bcc69961524cfbf8d214227d1d34012840b5b
park097 <alslalslclq24@naver.com>
2023-11-17 14:56:41 +0900

    store

---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: StorePage(),
+    );
+  }
+}
+
+class StorePage extends StatelessWidget {
+  StorePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding:
+                  const EdgeInsets.only(top: 1), //엣지는 가장자리 INSET은 앱이 사용할수 없는 부분
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("Woman", style: TextStyle(fontWeight: FontWeight.bold)),
+                  Text("Kids", style: TextStyle(fontWeight: FontWeight.bold)),
+                  Text("Shoes", style: TextStyle(fontWeight: FontWeight.bold)),
+                  Text("Bag", style: TextStyle(fontWeight: FontWeight.bold)),
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Image.asset(
+                "assets/bag.jpeg",
+                fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(
+              height: 1,
+            ),
+            Expanded(
+              flex: 1,
+              child: Image.asset(
+                "assets/cloth.jpeg",
+                fit: BoxFit.cover,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
